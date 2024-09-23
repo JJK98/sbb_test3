8a51495b31960a116af6740448a70c8d8189fed7
JJk <wnsrb1594@naver.com>
2024-09-23 13:16:41 +0900

    2-2

--- src/main/java/com/example/sbb_test4/article/ArticleController.java
+++ src/main/java/com/example/sbb_test4/article/ArticleController.java
@@ -2,11 +2,14 @@ package com.example.sbb_test4.article;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -55,5 +58,12 @@ public class ArticleController {
         Article article = this.articleService.getArticle(id);
         this.articleService.modify(article,articleForm.getTitle(), articleForm.getContent());
         return String.format("redirect:/article/detail/%s", id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public String articleDelete(@PathVariable("id") Integer id) {
+        Article article = this.articleService.getArticle(id);
+        this.articleService.delete(article);
+        return "redirect:/";
     }
 }
