0e74a379ca9482687e6fc5d3ded4a6859a08d215
JJk <wnsrb1594@naver.com>
2024-09-23 09:35:50 +0900

    1-1

---
+++ src/main/java/com/example/sbb_test4/SbbTest4Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb_test4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbTest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbTest4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbb_test4/SbbTest4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb_test4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbTest4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
